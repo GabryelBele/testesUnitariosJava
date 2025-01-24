098d11b9cf3a17e9961ae47bc46ffaf7e2638f42
GabryelBele <belegabryel19@gmail.com>
2025-01-24 15:57:06 -0300

    Create Domain User

--- src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
+++ src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
@@ -3,11 +3,16 @@ package br.com.dicasdeumdev.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.dicasdeumdev.api.domain.User;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+		User user = new User(null, "gabryel", "gabryel@email.com", "123");
+		System.out.println(user);
 	}
 
 }
